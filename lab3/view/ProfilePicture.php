100b8841d5817e9854e6ba675e31c5dd423a9110
Nazia <106931407+Nazia-1@users.noreply.github.com>
2022-07-06 21:57:57 +0600

    Add files via upload

---
+++ lab3/view/ProfilePicture.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>  
+          
+	<title>Profile Picture</title>
+</head>
+<tr>
+        <td colspan="2">
+			<fieldset>
+                <legend><b>Profile Picture</b></legend>
+                        <body>	
+                            <form action="upload.php" method="post">
+                                <img src="blank-picture-holder.png" alt="PP" width="270" height="300"><br>
+                                <div class="form-row">
+                                <div>Choose Image file:</div>
+                                <div>
+                                <input type="file" name="fileToUpload" id="fileToUpload">
+                                </div>
+                                </div>
+                                <input type="submit" value="Submit" name="submit">
+                            </form>
+            </fieldset>
+        </td>
+</tr>
+
+                    </body>
+</html>
